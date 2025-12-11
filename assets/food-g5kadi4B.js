18d3d12632391d95e03d2c7b5b39cc8bcf6b514f
blackreaper228 <ntsvetnikov@gmail.com>
2025-12-11 23:13:16 +0300

    Updates

---
+++ assets/food-g5kadi4B.js
@@ -0,0 +1,1 @@
+import"./main-CXm5JoA4.js";/* empty css                     */import{a as o}from"./photoGallery-mGZE889x.js";o();
--- assets/magazine-C1po6omL.js
+++
@@ -1,1 +0,0 @@
-import"./main-BpJeUbI4.js";import{a}from"./photoGallery-DTqlRJwg.js";a();
